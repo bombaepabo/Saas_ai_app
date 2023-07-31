3b2693f561c495001803ef2ae011f95005b81222
phakkhapon aksonchanya <55224271+bombaepabo@users.noreply.github.com>
2023-07-31 15:28:48 +0700

    added authentication with clerk

---
+++ sass-bb/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage = () => {
+  return (
+    <div>
+    <p>Dashboard page (Protected)</p>
+    </div>
+  )
+}
+export default DashboardPage;
