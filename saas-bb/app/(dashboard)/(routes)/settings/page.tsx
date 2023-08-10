471ba8a82c5b9214d1d2a8a35d2150f44c99a7ee
phakkhapon aksonchanya <55224271+bombaepabo@users.noreply.github.com>
2023-08-10 15:54:26 +0700

    updated settings page

--- saas-bb/app/(dashboard)/(routes)/settings/page.tsx
+++ saas-bb/app/(dashboard)/(routes)/settings/page.tsx
@@ -15,7 +15,7 @@ const SettingPage = async () =>{
             bgColor="bg-gray-700/10"/>
             <div className="px-4 lg:px-8 space-y-4">
                 <div className="text-muted-foreground text-sm">
-                    {isPro ? "You are currently on a pro plan" : "You are currentlyon a free plan"}
+                    {isPro ? "You are currently on a pro plan" : "You are currently on a free plan"}
                 </div>
                 <SubscriptionButton isPro={isPro}/>
             </div>
